06e3a38a5e83c6963d19e9973640a7b593584d09
thyzlinn1806 <thuylinh061104@gmail.com>
2025-04-06 00:51:24 +0700

    thlinh them thanh tim kiem

--- app/Http/Controllers/OopsController.php
+++ app/Http/Controllers/OopsController.php
@@ -21,7 +21,23 @@ class OopsController extends Controller
 
     function chitiet($id)
     {
-        $data = DB::select("select * from products where id = ?",[$id])[0];
+        $data = DB::table('products')->where('id', $id)->first();
+        
         return view("giaodiennguoidung.chitiet", compact("data"));
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        // Tìm kiếm sản phẩm theo tên, hãng
+        $data = DB::select(
+            "SELECT p.id as product_id, p.*, b.brand_name 
+             FROM products p
+             JOIN phone_brands b ON p.phone_brand_id = b.id
+             WHERE p.product_name LIKE ? OR b.brand_name LIKE ?",
+            ['%' . $query . '%', '%' . $query . '%']
+        );
+        return view('giaodiennguoidung.results', compact('data', 'query'));
     }
 }
